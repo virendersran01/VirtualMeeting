a7197ba93811e1a7a382eadd59bed0bf8178e227
virendersran01 <virendersran01@gmail.com>
2020-07-23 13:07:29 +0530

    Done Sign Up Activity.

--- app/src/main/java/com/virtualstudios/virtualmeeting/SignInActivity.java
+++ app/src/main/java/com/virtualstudios/virtualmeeting/SignInActivity.java
@@ -2,7 +2,9 @@ package com.virtualstudios.virtualmeeting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        findViewById(R.id.textSignUp).setOnClickListener(view -> startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
     }
 }
--- app/src/main/java/com/virtualstudios/virtualmeeting/SignUpActivity.java
+++ app/src/main/java/com/virtualstudios/virtualmeeting/SignUpActivity.java
@@ -3,6 +3,7 @@ package com.virtualstudios.virtualmeeting;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        findViewById(R.id.imageBack).setOnClickListener(view -> onBackPressed());
+        findViewById(R.id.textSignIn).setOnClickListener(view -> onBackPressed());
     }
 }
