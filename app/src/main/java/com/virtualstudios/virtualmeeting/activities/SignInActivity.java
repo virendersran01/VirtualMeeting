b4ae72e537748bc7ff9a7c5e5e48cc4b37769c11
virendersran01 <virendersran01@gmail.com>
2020-07-24 13:08:31 +0530

    Done Sign Up and Sign In using firebase.

--- app/src/main/java/com/virtualstudios/virtualmeeting/activities/SignInActivity.java
+++ app/src/main/java/com/virtualstudios/virtualmeeting/activities/SignInActivity.java
@@ -1,20 +1,87 @@
 package com.virtualstudios.virtualmeeting.activities;
 
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.virtualstudios.virtualmeeting.R;
+import com.virtualstudios.virtualmeeting.utilities.Constants;
+import com.virtualstudios.virtualmeeting.utilities.PreferenceManager;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private EditText inputEmail, inputPassword;
+    private MaterialButton buttonSignIn;
+    private ProgressBar signInProgressBar;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        preferenceManager = new PreferenceManager(getApplicationContext());
+
+        if (preferenceManager.getBoolean(Constants.KEY_IS_SIGNED_IN)){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
 
         findViewById(R.id.textSignUp).setOnClickListener(view -> startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
+        buttonSignIn = findViewById(R.id.buttonSignIn);
+        signInProgressBar = findViewById(R.id.signInProgressBar);
+
+        buttonSignIn.setOnClickListener(view -> {
+            if (inputEmail.getText().toString().trim().isEmpty()) {
+                Toast.makeText(SignInActivity.this, "Enter email", Toast.LENGTH_SHORT).show();
+            } else if (!Patterns.EMAIL_ADDRESS.matcher(inputEmail.getText().toString()).matches()) {
+                Toast.makeText(SignInActivity.this, "Enter valid email", Toast.LENGTH_SHORT).show();
+            } else if (inputPassword.getText().toString().trim().isEmpty()) {
+                Toast.makeText(SignInActivity.this, "Enter password", Toast.LENGTH_SHORT).show();
+            }else {
+                signIn();
+            }
+        });
+    }
+
+    private void signIn(){
+
+        buttonSignIn.setVisibility(View.INVISIBLE);
+        signInProgressBar.setVisibility(View.VISIBLE);
+
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .whereEqualTo(Constants.KEY_EMAIL, inputEmail.getText().toString())
+                .whereEqualTo(Constants.KEY_PASSWORD, inputPassword.getText().toString())
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful() && task.getResult() != null && task.getResult().getDocuments().size() > 0){
+                        DocumentSnapshot documentSnapshot = task.getResult().getDocuments().get(0);
+                        preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN, true);
+                        preferenceManager.putSting(Constants.KEY_FIRST_NAME, documentSnapshot.getString(Constants.KEY_FIRST_NAME));
+                        preferenceManager.putSting(Constants.KEY_LAST_NAME, documentSnapshot.getString(Constants.KEY_LAST_NAME));
+                        preferenceManager.putSting(Constants.KEY_EMAIL, documentSnapshot.getString(Constants.KEY_EMAIL));
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                    }else {
+                        signInProgressBar.setVisibility(View.INVISIBLE);
+                        buttonSignIn.setVisibility(View.VISIBLE);
+                        Toast.makeText(SignInActivity.this, "Unable sign in", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
 
     }
 }
---
+++ app/src/main/java/com/virtualstudios/virtualmeeting/utilities/Constants.java
@@ -0,0 +1,13 @@
+package com.virtualstudios.virtualmeeting.utilities;
+
+public class Constants {
+
+    public static final String KEY_COLLECTION_USERS = "users";
+    public static final String KEY_FIRST_NAME = "first_name";
+    public static final String KEY_LAST_NAME = "last_name";
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_PASSWORD = "password";
+
+    public static final String KEY_PREFERENCE_NAME = "videoMeetingPreference";
+    public static final String KEY_IS_SIGNED_IN = "isSignedIn";
+}
