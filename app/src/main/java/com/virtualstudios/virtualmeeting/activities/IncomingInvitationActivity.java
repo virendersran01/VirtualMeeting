889168f7b6cb2027ed19408eb3acb86e884058f2
virendersran01 <virendersran01@gmail.com>
2020-08-02 21:19:03 +0530

    added initiate invitation code.

--- app/src/main/java/com/virtualstudios/virtualmeeting/activities/IncomingInvitationActivity.java
+++ app/src/main/java/com/virtualstudios/virtualmeeting/activities/IncomingInvitationActivity.java
@@ -3,8 +3,11 @@ package com.virtualstudios.virtualmeeting.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.virtualstudios.virtualmeeting.R;
+import com.virtualstudios.virtualmeeting.utilities.Constants;
 
 public class IncomingInvitationActivity extends AppCompatActivity {
 
@@ -12,5 +15,28 @@ public class IncomingInvitationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_incoming_invitation);
+
+        ImageView imageMeetingType = findViewById(R.id.imageMeetingType);
+        String meetingType = getIntent().getStringExtra(Constants.REMOTE_MSG_MEETING_TYPE);
+
+        if (meetingType != null){
+            if (meetingType.equals("video")){
+                imageMeetingType.setImageResource(R.drawable.ic_video);
+            }else {
+                imageMeetingType.setImageResource(R.drawable.ic_audio);
+            }
+        }
+
+        TextView textFirstChar = findViewById(R.id.textFirstChar);
+        TextView textUsername = findViewById(R.id.textUsername);
+        TextView textEmail = findViewById(R.id.textEmail);
+
+        String firstName = getIntent().getStringExtra(Constants.KEY_FIRST_NAME);
+        if (firstName != null){
+            textFirstChar.setText(firstName.substring(0, 1));
+        }
+
+        textUsername.setText(String.format("%s %s", firstName, getIntent().getStringExtra(Constants.KEY_LAST_NAME)));
+        textEmail.setText(getIntent().getStringExtra(Constants.KEY_EMAIL));
     }
 }
